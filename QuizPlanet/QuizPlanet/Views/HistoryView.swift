09c8b1896f9c695a89f2190fbe74b89c1ed5ba8f
lessia2020 <63756124+lessia2020@users.noreply.github.com>
2023-02-01 21:14:58 +0100

    Update HistoryView.swift

--- QuizPlanet/QuizPlanet/Views/HistoryView.swift
+++ QuizPlanet/QuizPlanet/Views/HistoryView.swift
@@ -8,8 +8,61 @@
 import SwiftUI
 
 struct HistoryView: View {
+    let bgColor = Color(red: 255/255, green: 242/255, blue: 242/255)
+    let buttonColor = Color(red: 229/255, green: 224/255, blue: 255/255)
+    let fontColor = Color(red: 114/255, green: 134/255, blue: 211/255)
     var body: some View {
-        Text("Hello, History")
+        ZStack {
+            bgColor.ignoresSafeArea()
+            
+            VStack(spacing: 60) {
+                Spacer()
+                Text("My question")
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 42))
+                    
+                //GridButton()
+                Spacer()
+                
+                HStack(spacing: 120) {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Answer A")
+                    }).padding()
+                        .background(buttonColor)
+                        .cornerRadius(12)
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Answer B")
+                    }).padding()
+                        .background(buttonColor)
+                        .cornerRadius(12)
+                    
+                }
+                HStack (spacing: 120) {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Answer C")
+                    }).padding()
+                      .background(buttonColor)
+                      .cornerRadius(12)
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Answer D")
+                    }).padding()
+                    .background(buttonColor)
+                    .cornerRadius(12)
+                }
+                Spacer()
+                
+                
+        }
+    }
     }
 }
 
